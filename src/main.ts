bccf93d0a30b30c82daa256f2e9a995936940cca
Smit2 <smitarcade@gmail.com>
2023-01-11 18:37:06 +0100

    fix: test 6
    Introduce a request id and a reference to latest request. Dismiss
    incoming responses other than from latest request.
    
    Remove timeouts which were used to mitigate the racing issue but are
    obsolete now.
    
    Reviewed-by: Simone

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
-  console.log('test 4');
-  console.log('test 5');
+  console.log('test 6');
 }
 bootstrap();
